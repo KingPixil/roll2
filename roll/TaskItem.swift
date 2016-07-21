ef697888700c0dfb03022f0dea61f5dfa0cec29a
Gopal Shah <simplecooldude1@gmail.com>
2016-07-21 10:04:55 -0700

    task item class

---
+++ roll/TaskItem.swift
@@ -0,0 +1,13 @@
+//
+//  TaskItem.swift
+//  roll
+//
+//  Created by Kabir Shah on 7/21/16.
+//  Copyright © 2016 Kabir Shah. All rights reserved.
+//
+
+import UIKit
+
+class TaskItem: NSObject {
+
+}
